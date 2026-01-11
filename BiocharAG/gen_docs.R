c19dc735a4312f0b27fd22f9cc1a489a0203206b
domwoolf <domwoolf@gmail.com>
2026-01-11 15:34:15 -0500

    Feat: Advanced Biochar Valuation, Wholesale Pricing, and Project Reorg

--- BiocharAG/gen_docs.R
+++
@@ -1,6 +0,0 @@
-
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools", repos = "http://cran.us.r-project.org")
-library(devtools)
-
-document()
-message("Documentation generated.")
